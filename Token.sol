012ecfa0e2804f7e807b18491723c078b1a01954
MariappanSeetha <mariseetha19@gmail.com>
2021-12-02 16:35:08 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("BUDNET_GRID", "BTN", 18) {
+    constructor () public TRC20Detailed("TOKEN_NAME", "CODE", 18) {
         _mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
     }
 }
